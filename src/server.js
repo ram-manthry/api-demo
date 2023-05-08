f1e63de3a702780a508f5d2d321fe69c549777dd
Ram Manthry <ram.manthry@gmail.com>
2023-05-08 18:38:58 +1200

    added prettier

--- src/server.js
+++ src/server.js
@@ -1,102 +1,102 @@
-import express from 'express';
-import env from 'dotenv';
+import express from 'express'
+import env from 'dotenv'
 
-env.config();
+env.config()
 
-const server = express();
-server.use(express.json());
+const server = express()
+server.use(express.json())
 
 let tasks = [
-	{
-		id: 1,
-		name: 'Mission 01',
-		description: 'Chatbot',
-		isCompleted: false,
-	},
-];
+  {
+    id: 1,
+    name: 'Mission 01',
+    description: 'Chatbot',
+    isCompleted: false,
+  },
+]
 
 server.get('/tasks', (req, res) => {
-	res.send(tasks);
-});
+  res.send(tasks)
+})
 
 server.post('/tasks', (req, res) => {
-	const name = req.body.name;
-	const description = req.body.description;
+  const name = req.body.name
+  const description = req.body.description
 
-	const newTask = {
-		id: tasks.length + 1,
-		name,
-		description,
-		isCompleted: false,
-	};
+  const newTask = {
+    id: tasks.length + 1,
+    name,
+    description,
+    isCompleted: false,
+  }
 
-	tasks.push(newTask);
+  tasks.push(newTask)
 
-	res.send(newTask);
-});
+  res.send(newTask)
+})
 
 server.get('/tasks/:id', (req, res) => {
-	const taskId = parseInt(req.params.id);
+  const taskId = parseInt(req.params.id)
 
-	const matchedTask = tasks.find((t) => t.id === taskId);
+  const matchedTask = tasks.find((t) => t.id === taskId)
 
-	if (!matchedTask) {
-		res.status(404).send('Task not found');
-	}
+  if (!matchedTask) {
+    res.status(404).send('Task not found')
+  }
 
-	res.send(matchedTask);
-});
+  res.send(matchedTask)
+})
 
 server.delete('/tasks/:id', (req, res) => {
-	const taskId = parseInt(req.params.id);
+  const taskId = parseInt(req.params.id)
 
-	const matchedTask = tasks.find((t) => t.id === taskId);
+  const matchedTask = tasks.find((t) => t.id === taskId)
 
-	if (!matchedTask) {
-		res.status(404).send('Task not found');
-	}
+  if (!matchedTask) {
+    res.status(404).send('Task not found')
+  }
 
-	tasks = tasks.filter((t) => t.id !== taskId);
-	res.send(`Task ${taskId} deleted successfully!`);
-});
+  tasks = tasks.filter((t) => t.id !== taskId)
+  res.send(`Task ${taskId} deleted successfully!`)
+})
 
 server.put('/tasks/:id', (req, res) => {
-	const taskId = parseInt(req.params.id);
+  const taskId = parseInt(req.params.id)
 
-	const name = req.body.name;
-	const description = req.body.description;
+  const name = req.body.name
+  const description = req.body.description
 
-	const matchedTask = tasks.find((t) => t.id === taskId);
+  const matchedTask = tasks.find((t) => t.id === taskId)
 
-	if (!matchedTask) {
-		res.status(404).send('Task not found');
-	}
+  if (!matchedTask) {
+    res.status(404).send('Task not found')
+  }
 
-	matchedTask.name = name;
-	matchedTask.description = description;
+  matchedTask.name = name
+  matchedTask.description = description
 
-	res.send(matchedTask);
-});
+  res.send(matchedTask)
+})
 
 server.patch('/tasks/:id', (req, res) => {
-	const taskId = parseInt(req.params.id);
+  const taskId = parseInt(req.params.id)
 
-	const name = req.body.name;
-	const description = req.body.description;
+  const name = req.body.name
+  const description = req.body.description
 
-	const matchedTask = tasks.find((t) => t.id === taskId);
+  const matchedTask = tasks.find((t) => t.id === taskId)
 
-	if (!matchedTask) {
-		res.status(404).send('Task not found');
-	}
+  if (!matchedTask) {
+    res.status(404).send('Task not found')
+  }
 
-	matchedTask.name = name ?? matchedTask.name;
-	matchedTask.description = description ?? matchedTask.description;
+  matchedTask.name = name ?? matchedTask.name
+  matchedTask.description = description ?? matchedTask.description
 
-	res.send(matchedTask);
-});
+  res.send(matchedTask)
+})
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
-	console.log(`server starterd on port ${PORT}`);
-});
+  console.log(`server starterd on port ${PORT}`)
+})
